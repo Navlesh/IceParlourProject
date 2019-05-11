aa6bec699b50d1eba90458a78f0b1a99e6d61d30
Navlesh Agarwal <navlesh.agr@gmail.com>
2019-05-12 00:54:36 +0530

    Initial Checkin

---
+++ IceParlour/IceParlourApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IceParlourApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
